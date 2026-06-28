257e508a42580d119b570bcb7d1b8bd17dd2ce9c
EC2 Default User <ec2-user@ip-172-31-35-9.ap-south-1.compute.internal>
2026-06-28 14:00:11 +0000

    Script is Improved

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "Updating system..."
 sudo dnf update -y
 
@@ -12,4 +14,12 @@ sudo systemctl start nginx
 echo "Enabling Nginx..."
 sudo systemctl enable nginx
 
+echo "Deploying website..."
+sudo cp ../website/index.html /usr/share/nginx/html/index.html
+
+echo "Restarting Nginx..."
+sudo systemctl restart nginx
+
+echo "==================================="
 echo "Nginx setup completed successfully!"
+echo "==================================="
